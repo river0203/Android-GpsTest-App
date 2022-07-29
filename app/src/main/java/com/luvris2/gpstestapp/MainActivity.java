338d88ca196582782cd40733ae3dfc975b9a4711
luvris2 <luvris2@naver.com>
2022-07-29 11:35:05 +0900

    add

---
+++ app/src/main/java/com/luvris2/gpstestapp/MainActivity.java
@@ -0,0 +1,69 @@
+package com.luvris2.gpstestapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    // GPS 사용을 위한 멤버 변수 선언
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            // GPS의 정보를 얻어 올 수 있는 메소드
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.i("MyLocation", "위도 : " + location.getLatitude());
+                Log.i("MyLocation", "경도 : " + location.getLongitude());
+            }
+        };
+
+        // 사용자가 앱에 GPS 사용 권한 부여하였는지 체크
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            return;
+        }
+        // requestLocationUpdates(String provider, long minTimeMs, float minDistanceM, LocationListener listener, Looper looper)
+        // 첫번째 파라미터 : Provider 지정 -> LocationManager.GPS_PROVIDER (GPS 사용 선언)
+        // 두번째 파라미터 : GPS 호출 시간 간격
+        // 세번째 파라미터 : GPS 호출 최소 이동거리
+        // 네번재 파라미터 : 로캐이션 리스너 이름
+        // 예시) 3초마다 위치 정보 업데이트
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                3000, -1, locationListener);
+    }
+
+    // 앱 권한 요청 설정 메소드
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 100) {
+            // 권한 요청
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            }
+        }
+    }
+}
